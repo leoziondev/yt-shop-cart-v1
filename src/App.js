525b89fda7bc660d7514ad47c569257a35176394
LeoZion <leozionsuporte@gmail.com>
2021-10-29 11:20:40 -0300

    component: Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Start Project
-    </div>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import { Container, Navbar, FormControl, Nav, Dropdown, Badge } from "react-bootstrap"
+
+import { FaShoppingCart } from 'react-icons/fa'
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/">Shopping Cart</Link>
+                </Navbar.Brand>
+                <Navbar.Text className="search">
+                    <FormControl
+                        style={{ width: 350 }}
+                        placeholder="Search a product"
+                        className="m-auto"
+                    />
+                </Navbar.Text>
+
+                <Nav>
+                    <Dropdown alignRight>
+                        <Dropdown.Toggle variant="primary">
+                            <FaShoppingCart color="white" fontSize="25px" />
+                            <Badge>{10}</Badge>
+                        </Dropdown.Toggle>
+    
+                        <Dropdown.Menu style={{ minWidth: 370 }}>
+                            <span style={{ padding: 10 }}>Cart is Empty!</span>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
