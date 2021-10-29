a602125c86c7bedd3c13e08cd34a86c5c9b787ce
LeoZion <leozionsuporte@gmail.com>
2021-10-29 12:30:49 -0300

    add faker and config routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home'
+import Cart from './components/Cart'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" component={Cart} />
+      </div>
     </BrowserRouter>
   );
 }
